cc840a0f7340a98249b312d981b2a4e4ac4440dd
Mario-S <spindizzy@outlook.com>
2024-07-26 11:47:07 +0200

    wip: test

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -2,8 +2,8 @@ use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn substitute(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn substitute(_param: String) -> PyResult<String> {
+    Ok("2".to_string())
 }
 
 /// A Python module implemented in Rust.
@@ -11,4 +11,16 @@ fn substitute(a: usize, b: usize) -> PyResult<String> {
 fn native(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(substitute, m)?)?;
     Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn substitute_with_full_math_expression() {
+        let result = substitute("#{1 + 1}".to_string());
+        assert_eq!("2", result.unwrap(), "expected an result for a match expression");
+    }
 }
